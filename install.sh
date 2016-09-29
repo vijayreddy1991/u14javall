ac9c94f78f37085929959f2b4c907efa0221638c
ankul-shippable <ankul@shippable.com>
2016-09-29 12:16:05 +0530

    check versions

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 sudo apt-get clean
-sudo mv /var/lib/apt/lists /tmp
+sudo mv /var/lib/apt/lists/* /tmp
 sudo mkdir -p /var/lib/apt/lists/partial
 sudo apt-get clean
 sudo apt-get update
@@ -12,13 +12,13 @@ unzip -qq gradle-3.1-all.zip -d /usr/local && rm -f gradle-3.1-all.zip
 ln -fs /usr/local/gradle-3.1/bin/gradle /usr/bin
 echo 'export PATH=$PATH:/usr/local/gradle-3.1/bin' >> $HOME/.bashrc
 
-echo "================ Installing apache-maven-3.2.5 ================="
+echo "================ Installing apache-maven-3.3.9 ================="
 sudo wget http://redrockdigimark.com/apachemirror/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 sudo tar xzf apache-maven-3.3.9-bin.tar.gz -C /usr/local && rm -f apache-maven-3.3.9-bin.tar.gz
 ln -fs /usr/local/apache-maven-3.3.9/bin/mvn /usr/bin
 echo 'export PATH=$PATH:/usr/local/apache-maven-3.3.9/bin' >> $HOME/.bashrc
 
-echo "================ Installing apache-ant-1.9.6 ================="
+echo "================ Installing apache-ant-1.9.7 ================="
 wget http://redrockdigimark.com/apachemirror/ant/binaries/apache-ant-1.9.7-bin.tar.gz
 tar xzf apache-ant-1.9.7-bin.tar.gz -C /usr/local && rm -f apache-ant-1.9.7-bin.tar.gz
 ln -fs /usr/local/apache-ant-1.9.7/bin/ant /usr/bin
