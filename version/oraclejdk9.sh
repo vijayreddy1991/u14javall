ff9aa1e9bbdfed1d732df8ad0b4546e2b2f0619c
Bharath Upadhya <1992bharath@gmail.com>
2017-10-25 21:36:14 +0530

    updates gradle & links sdkmanager to /usr/bin

--- version/oraclejdk9.sh
+++ version/oraclejdk9.sh
@@ -5,8 +5,3 @@ echo oracle-java9-installer shared/accepted-oracle-license-v1-1 select true | de
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
 apt-get install -y oracle-java9-installer
-update-alternatives --set java /usr/lib/jvm/java-9-oracle/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-9-oracle/bin/javac
-update-alternatives --set javaws /usr/lib/jvm/java-9-oracle/bin/javaws
-echo 'export JAVA_HOME=/usr/lib/jvm/java-9-oracle' >> $HOME/.bashrc
-echo 'export PATH=$PATH:/usr/lib/jvm/java-9-oracle/bin' >> $HOME/.bashrc
