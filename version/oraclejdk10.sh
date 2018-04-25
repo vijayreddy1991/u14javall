20395829de2b7e963e148d85f89eeee87d51e2b7
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-04-25 13:48:29 +0530

    update the apt-get

--- version/oraclejdk10.sh
+++ version/oraclejdk10.sh
@@ -3,5 +3,5 @@
 echo "================ Installing oracle-java10-installer ================="
 echo oracle-java10-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 sudo add-apt-repository -y ppa:linuxuprising/java
-sudo apt update
-sudo apt install oracle-java10-installer
+sudo apt-get update
+sudo apt-get install oracle-java10-installer
