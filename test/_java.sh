518ae7eeafc832f2c15630d5d0ebaed7fdf1ac8e
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-23 15:04:23 +0530

    Update _java.sh

--- test/_java.sh
+++ test/_java.sh
@@ -1,31 +1,24 @@
 #!/bin/bash -e
 
 echo "============================ JDK versions ==============================="
-echo "update-alternatives --list java"
-update-alternatives --list java
-printf "\n\n"
 
-echo "update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java"
-update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+
+shipctl jdk set openjdk7
 java -version
 printf "\n"
 
-echo "update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+shipctl jdk set openjdk8
 java -version
 printf "\n"
 
-echo "update-alternatives --set java /usr/lib/jvm/java-9-openjdk-amd64/bin/java"
-update-alternatives --set java /usr/lib/jvm/java-9-openjdk-amd64/bin/java
+shipctl jdk set openjdk9
 java -version
 printf "\n"
 
-echo "update-alternatives --set java /usr/lib/jvm/java-9-oracle/bin/java"
-update-alternatives --set java /usr/lib/jvm/java-9-oracle/bin/java
+shipctl jdk set oraclejdk8
 java -version
 printf "\n"
 
-echo "update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java"
-update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
+shipctl jdk set oraclejdk10
 java -version
 printf "\n"
