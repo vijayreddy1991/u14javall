054486a1a1d2a856d4da0ec2ab27039340d7d805
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-08-16 16:28:11 +0530

    Update openjdk9.sh

--- version/openjdk9.sh
+++ version/openjdk9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-echo "================ Installing openjdk-10-jdk ================="
+echo "================ Installing openjdk-9-jdk ================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 apt-get install -y -q openjdk-9-jdk
